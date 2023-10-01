15351d5940d793d27bf3df687e84f8b73bd9126b
xiaosongfu <xiaosong.fu@outlook.com>
2023-10-01 10:53:39 +0800

    refactor normalize() function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import { Buffer } from "buffer";
 import { keccak_256 } from "@noble/hashes/sha3";
 import { bytesToHex } from "@noble/hashes/utils";
-import { ens_normalize } from "@adraffy/ens-normalize";
 
-function normalize(name: string): string {
-  return ens_normalize(name);
+// returns the normalized version of a sns name
+//
+// ASCII:
+// 48~57 0-9
+// 65~90 A-Z
+// 97~122 a-z
+export function normalize(name: string): [boolean, string] {
+  const lowerCaseName = name.toLowerCase();
+  for (let i = 0; i < lowerCaseName.length; i++) {
+    const char = lowerCaseName.charCodeAt(i);
+    if ((char >= 48 && char <= 57) || (char >= 97 && char <= 122)) {
+      continue;
+    }
+    return [false, ""];
+  }
+
+  return [true, lowerCaseName];
 }
 
-function namehash(name: string): string {
+// returns the namehash value of a sns name
+export function namehash(name: string): string {
   let node: string =
     "0000000000000000000000000000000000000000000000000000000000000000";
 
@@ -19,5 +34,3 @@ function namehash(name: string): string {
 
   return "0x" + node;
 }
-
-export { normalize, namehash };
