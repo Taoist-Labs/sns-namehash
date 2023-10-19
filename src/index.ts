8c938c25b35f7974c0fef908a55f90312cb75a7e
xiaosongfu <xiaosong.fu@outlook.com>
2023-10-19 13:30:01 +0800

    feat: normalize function handle special chars

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,34 @@ import { bytesToHex } from "@noble/hashes/utils";
 // returns the normalized version of a sns name
 //
 // ASCII:
-// 48~57 0-9
-// 65~90 A-Z
-// 97~122 a-z
-// 46 . (dot)
+// 0-9: 48~57
+// a-z: 97~122
+// !  : 33
+// $  : 36
+// (  : 40
+// )  : 41
+// *  : 42
+// +  : 43
+// -  : 45
+// .  : 46 (dot)
+// _  : 95
 export function normalize(name: string): [boolean, string] {
   const lowerCaseName = name.toLowerCase();
   for (let i = 0; i < lowerCaseName.length; i++) {
     const char = lowerCaseName.charCodeAt(i);
     if (
-      (char >= 48 && char <= 57) ||
       (char >= 97 && char <= 122) ||
-      char == 46
+      (char >= 48 && char <= 57) ||
+      char == 33 ||
+      char == 36 ||
+      // char == 40 ||
+      // char == 41 ||
+      // char == 42 ||
+      // char == 43 ||
+      (char >= 40 && char <= 43) ||
+      char == 45 ||
+      char == 46 ||
+      char == 95
     ) {
       continue;
     }
@@ -34,7 +50,7 @@ export function namehash(name: string): string {
   const s = name.split(".");
   for (let i = s.length - 1; i >= 0; i--) {
     const hash = bytesToHex(keccak_256(s[i]));
-    node = bytesToHex(keccak_256(Buffer.from(node + hash, "HEX")));
+    node = bytesToHex(keccak_256(Buffer.from(node + hash, "hex")));
   }
 
   return "0x" + node;
